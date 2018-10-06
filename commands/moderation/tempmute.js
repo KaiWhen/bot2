c1c93193e4f3d22ab9c943495d6780a1ccf1250b
KaiWhen <wangkw523@gmail.com>
2018-10-06 18:13:49 +0100

    asdfsadfsadf

--- commands/moderation/tempmute.js
+++ commands/moderation/tempmute.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 const ms = require("ms");
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const mutereport = require("../mongotest.js");
 module.exports.run = async(bot, message, args) => {
     
