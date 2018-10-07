c912db64ee61f2d293c48b444d8ecb19213c14db
KaiWhen <wangkw523@gmail.com>
2018-10-07 08:24:13 +0100

    gfhgfhgfh

--- index.js
+++ index.js
@@ -52,8 +52,7 @@ bot.on("message", async message => {
     let cmd = messageArray[0];
     let prefix = botconfig.prefix;
     let args = messageArray.slice(1);
-    let commandfile = bot.commands.get(cmd.slice(prefix.length));
-    if(commandfile) commandfile.run(bot, message, args);
+    
 
     if(message.content.startsWith(prefix)){
         let commandfile = bot.commands.get(cmd.slice(prefix.length));
