748acfb527edfe8a21b4da1099eb8ba72f0603b1
KaiWhen <wangkw523@gmail.com>
2018-10-07 19:49:40 +0100

    gfdgfdgfdgfdg

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -13,6 +13,19 @@ module.exports.run = async(bot, message, args) => {
         if(err) console.log(err);
         let dailyEmbed = new Discord.RichEmbed()
         .setColor("#FFDF00")
+        if(!money){
+            const newMoney = new Money({
+                _id: mongoose.Types.ObjectId(),
+                userID: message.author.id,
+                username: message.author.username,
+                serverID: message.guild.id,
+                money: 500,
+                prevDaily: "not collected"
+            })
+            newMoney.save()
+            .then(result => console.log(result))
+            .catch(err => console.log(err));
+        }
         if(!money.prevDaily) money.prevDaily = "not collected";
         if(money.prevDaily != moment().format('L')){
             money.prevDaily = moment().format('L');
