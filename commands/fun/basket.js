ce7656b71ecf345004715e742d8094fe3ee87a04
KaiWhen <wangkw523@gmail.com>
2018-10-09 00:43:33 +0100

    ljoihp

--- commands/fun/basket.js
+++ commands/fun/basket.js
@@ -1,12 +1,20 @@
 const Discord = require("discord.js");
-
+const userData = require("../../models/userData.js");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 module.exports.run = async(bot, message, args) => {
    
-
     
+    message.reply("you just threw a ball at the basket and...");
+    setTimeout(2000, function(){
+        let rnd = Math.floor(Math.random()*10);
+        if(rnd === 6){
+            message.reply("scored!");
+        }else{
+            message.reply("missed! Hard luck!");
+        }
+    });
     
 }
 
