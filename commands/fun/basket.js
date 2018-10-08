a773ec7ef746ed3773c742fd67efb0f6ba646d8b
KaiWhen <wangkw523@gmail.com>
2018-10-09 00:46:23 +0100

    jkhgf

--- commands/fun/basket.js
+++ commands/fun/basket.js
@@ -7,14 +7,14 @@ module.exports.run = async(bot, message, args) => {
    
     
     message.reply("you just threw a ball at the basket and...");
-    setTimeout(2000, function(){
+    setTimeout(function(){
         let rnd = Math.floor(Math.random()*10);
         if(rnd === 6){
             message.reply("scored!");
         }else{
             message.reply("missed! Hard luck!");
         }
-    });
+    }, 2000);
     
 }
 
