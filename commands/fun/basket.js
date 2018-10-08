3e2595016813ed780e0a1e7523f667623d675e21
KaiWhen <wangkw523@gmail.com>
2018-10-09 00:49:18 +0100

    oihohouih

--- commands/fun/basket.js
+++ commands/fun/basket.js
@@ -8,11 +8,11 @@ module.exports.run = async(bot, message, args) => {
     
     message.reply("you just threw a ball at the basket and...");
     setTimeout(function(){
-        let rnd = Math.floor(Math.random()*10);
+        let rnd = Math.floor(Math.random()*5)+6;
         if(rnd === 6){
-            message.reply("scored!");
+            return message.reply("scored!");
         }else{
-            message.reply("missed! Hard luck!");
+            return message.reply("missed! Hard luck!");
         }
     }, 2000);
     
