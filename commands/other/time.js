bfa17c2f9493805a130cb751924e4302cddecdc1
KaiWhen <wangkw523@gmail.com>
2018-10-10 00:17:37 +0100

    hygf

--- commands/other/time.js
+++ commands/other/time.js
@@ -3,9 +3,8 @@ const moment = require("moment");
 
 module.exports.run = async(bot, message, args) => {
 
-    let time = moment().get('month').get('date').get('hour').get('minute');
-    let showtime = moment(time).format("M D H m");
-    message.channel.send(time);
+    
+    message.channel.send(moment().format("L LTS"));
 }
 
 module.exports.help = {
