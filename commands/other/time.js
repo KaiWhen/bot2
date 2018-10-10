cbfa52b820306bd50b633b003f0753d381de5f26
KaiWhen <wangkw523@gmail.com>
2018-10-10 14:26:01 +0100

    aerghqergqerg

--- commands/other/time.js
+++ commands/other/time.js
@@ -5,7 +5,9 @@ module.exports.run = async(bot, message, args) => {
 
     let a = moment().format("L LT");
     let b = moment("10/10/2018 13:12:23 PM");
-    message.channel.send(parseInt(b.from(a)));
+    //message.channel.send(parseInt(b.from(a)));
+    let added = b.add(5, 'seconds');
+    message.channel.send(added);
 }
 
 module.exports.help = {
