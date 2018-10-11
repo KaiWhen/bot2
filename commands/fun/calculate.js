1e7883052b97333b79a297b5efa126f4d3db4c84
KaiWhen <wangkw523@gmail.com>
2018-10-11 21:31:17 +0100

    yjutyjufu

--- commands/fun/calculate.js
+++ commands/fun/calculate.js
@@ -2,9 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
     
+    let x = parseInt(args[0]);
+    let y = parseInt(args[2]);
     if(message.content.includes("x")) return message.reply("Please use '*' instead of 'x' for multiplication");
     //if(args[1]) return message.reply("Don't leave spaces!");
-    message.channel.send(eval(args));
+    let answer = eval(args);
+    message.channel.send(answer);
 }
 
 module.exports.help = {
