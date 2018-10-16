8d431a6780ebfa72a1d520d8fab06ed635d4c19e
KaiWhen <wangkw523@gmail.com>
2018-10-16 17:19:37 +0100

    fdhshsh

--- commands/game/invtest.js
+++ commands/game/invtest.js
@@ -33,6 +33,10 @@ module.exports.run = async(bot, message, args) => {
         let amountkey = {"amount": 0}
         let copper = items.ore[0];
         inv.ore.push(items.ore[0]);
+        inv.ore.map(function(el) {
+            let o = Object.assign({}, el);
+            o.amount = 0;
+          });
         invData.findOneAndUpdate({userID: message.author.id}, {$push: {ore: copper}});
         invData.findOneAndUpdate({userID: message.author.id}, {$push: {ore: amountkey}});
         inv.save()
