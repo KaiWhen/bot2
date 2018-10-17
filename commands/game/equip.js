f14cf35ff5ab6b4227b68db8b71dad1c21f4e548
KaiWhen <wangkw523@gmail.com>
2018-10-17 01:29:48 +0100

    ioubyv

--- commands/game/equip.js
+++ commands/game/equip.js
@@ -70,21 +70,21 @@ module.exports.run = async(bot, message, args) => {
                 return message.channel.send(`**Equipped the Copper Sword.**`);
             }
             else if(material === "iron" && tool === "pickaxe"){
-                if(!inv.iron.pick) return message.reply("You do not own a Iron Pickaxe.");
+                if(!inv.iron.pick) return message.reply("You do not own an Iron Pickaxe.");
                 inv.pick = "iron pickaxe";
                 inv.save()
                 .catch(err => console.log(err));
                 return message.channel.send(`**Equipped the Iron Pickaxe.**`);
             }
             else if(material === "iron" && tool === "axe"){
-                if(!inv.iron.axe) return message.reply("You do not own a Iron Axe.");
+                if(!inv.iron.axe) return message.reply("You do not own an Iron Axe.");
                 inv.axe = "iron axe";
                 inv.save()
                 .catch(err => console.log(err));
                 return message.channel.send(`**Equipped the Iron Axe.**`);
             }
             else if(material === "iron" && tool === "sword"){
-                if(!inv.iron.weapon) return message.reply("You do not own a Iron Sword.");
+                if(!inv.iron.weapon) return message.reply("You do not own an Iron Sword.");
                 inv.weapon = "iron sword";
                 inv.save()
                 .catch(err => console.log(err));
