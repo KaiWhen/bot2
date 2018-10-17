006768d944c3e91627ccaba121ea527d86b060e1
KaiWhen <wangkw523@gmail.com>
2018-10-17 19:31:53 +0100

    awrgareg

--- commands/game/mine.js
+++ commands/game/mine.js
@@ -5,20 +5,24 @@ const mongoose = require("mongoose");
 const ms = require("parse-ms");
 const ratelimitMap = new Map();
 
-module.exports.run = async(bot, message, args) => {
+module.exports.run = async (bot, message, args) => {
 
 
     const ratelimit = ratelimitMap.get(message.author.id)
-    if(ratelimit !== null && (Date.now() - ratelimit) < 0 ){
+    if (ratelimit !== null && (Date.now() - ratelimit) < 0) {
         let timeObj = ms((ratelimit - Date.now()));
-        return message.reply(`You must wait ${timeObj.minutes} min(s) ${timeObj.seconds} sec(s) until you are finished!`);
+        let statEmbed = new Discord.RichEmbed()
+            .setTitle("Mining")
+            .setThumbnail(`${message.author.displayAvatarURL}`)
+            .addField("Time Remaining", `\`\`\`${timeObj.minutes} min(s) ${timeObj.seconds} sec(s)\`\`\``);
+        return message.channel.send(statEmbed);
     }
-    
+
     charData.findOne({
         userID: message.author.id
     }, (err, char) => {
-        if(err) console.log(err);
-        if(!char){
+        if (err) console.log(err);
+        if (!char) {
             const newChar = new charData({
                 _id: mongoose.Types.ObjectId(),
                 userID: message.author.id,
@@ -42,8 +46,8 @@ module.exports.run = async(bot, message, args) => {
                 active: false
             })
             newChar.save()
-            .then(result => console.log(result))
-            .catch(err => console.log(err));
+                .then(result => console.log(result))
+                .catch(err => console.log(err));
             return message.channel.send("**Please try again**");
         }
 
@@ -51,8 +55,8 @@ module.exports.run = async(bot, message, args) => {
         invData.findOne({
             userID: message.author.id
         }, (err, inv) => {
-            if(err) console.log(err);
-            if(!inv){
+            if (err) console.log(err);
+            if (!inv) {
                 const newInv = new invData({
                     _id: mongoose.Types.ObjectId(),
                     userID: message.author.id,
@@ -64,104 +68,220 @@ module.exports.run = async(bot, message, args) => {
                     pickdur: 0,
                     axedur: 100,
                     weapondam: 0,
-                    copper: {ore: 0, bar: 0, weapon: 0, pick: 0, axe: 0},
-                    iron: {ore: 0, bar: 0, weapon: 0, pick: 0, axe: 0},
-                    silver: {ore: 0, bar: 0, weapon: 0, pick: 0, axe: 0},
-                    nickel: {ore: 0, bar: 0, weapon: 0, pick: 0, axe: 0},
-                    gold: {ore: 0, bar: 0, weapon: 0, pick: 0, axe: 0},
-                    platinum: {ore: 0, bar: 0, weapon: 0, pick: 0, axe: 0},
-                    ruthenium: {ore: 0, bar: 0, weapon: 0, pick: 0, axe: 0},
-                    titanium: {ore: 0, bar: 0, weapon: 0, pick: 0, axe: 0},
-                    molybdenum: {ore: 0, bar: 0, weapon: 0, pick: 0, axe: 0},
-                    rhenium: {bar: 0, weapon: 0, pick: 0, axe: 0},
-                    iridium: {bar: 0, weapon: 0, pick: 0, axe: 0},
-                    osmium: {bar: 0, weapon: 0, pick: 0, axe: 0},
-                    diamond: {bar: 0, weapon: 0, pick: 0, axe: 0}
+                    copper: {
+                        ore: 0,
+                        bar: 0,
+                        weapon: 0,
+                        pick: 0,
+                        axe: 0
+                    },
+                    iron: {
+                        ore: 0,
+                        bar: 0,
+                        weapon: 0,
+                        pick: 0,
+                        axe: 0
+                    },
+                    silver: {
+                        ore: 0,
+                        bar: 0,
+                        weapon: 0,
+                        pick: 0,
+                        axe: 0
+                    },
+                    nickel: {
+                        ore: 0,
+                        bar: 0,
+                        weapon: 0,
+                        pick: 0,
+                        axe: 0
+                    },
+                    gold: {
+                        ore: 0,
+                        bar: 0,
+                        weapon: 0,
+                        pick: 0,
+                        axe: 0
+                    },
+                    platinum: {
+                        ore: 0,
+                        bar: 0,
+                        weapon: 0,
+                        pick: 0,
+                        axe: 0
+                    },
+                    ruthenium: {
+                        ore: 0,
+                        bar: 0,
+                        weapon: 0,
+                        pick: 0,
+                        axe: 0
+                    },
+                    titanium: {
+                        ore: 0,
+                        bar: 0,
+                        weapon: 0,
+                        pick: 0,
+                        axe: 0
+                    },
+                    molybdenum: {
+                        ore: 0,
+                        bar: 0,
+                        weapon: 0,
+                        pick: 0,
+                        axe: 0
+                    },
+                    rhenium: {
+                        bar: 0,
+                        weapon: 0,
+                        pick: 0,
+                        axe: 0
+                    },
+                    iridium: {
+                        bar: 0,
+                        weapon: 0,
+                        pick: 0,
+                        axe: 0
+                    },
+                    osmium: {
+                        bar: 0,
+                        weapon: 0,
+                        pick: 0,
+                        axe: 0
+                    },
+                    diamond: {
+                        bar: 0,
+                        weapon: 0,
+                        pick: 0,
+                        axe: 0
+                    }
                 })
                 newInv.save()
-                .then(result => console.log(result))
-                .catch(err => console.log(err));
+                    .then(result => console.log(result))
+                    .catch(err => console.log(err));
                 return message.channel.send("**Please try again**");
             }
 
-        if(char.active === false){
-        char.active = true;
-        char.save()
-        .then(result => console.log(result))
-        .catch(err => console.log(err));
-        if(inv.pick === "Stone Pickaxe"){
-            inv.pickspeed = 250;
-            inv.save()
-            .then(result => console.log(result))
-            .catch(err => console.log(err));
-        }
-        else if(inv.pick === "Copper Pickaxe"){
-            inv.pickspeed = 500;
-            inv.save()
-            .then(result => console.log(result))
-            .catch(err => console.log(err));
-        }
-        let minexprnd = Math.floor(Math.random()*4);
-        let minexpgain = minexprnd + char.minelvl;
-        let nextminelvl = Math.floor(Math.pow(char.minelvl, 2.6));
-        let minespeed = 600250 - inv.pickspeed;
-
-
-        let mineEmbed = new Discord.RichEmbed()
-        .setTitle("Mining")
-        .setColor("#855e42")
-        .setDescription("You are off to the mines for a while!")
-        .addField("Pickaxe", `${inv.pick}`);
-        message.channel.send(mineEmbed);
-        
-
-        let mineGainEmbed = new Discord.RichEmbed()
-        .setAuthor("Mining")
-        .setTitle("**You are back from mining!**")
-        .setColor("#855e42");
-            
-
-        setTimeout(() => {
-            if(inv.pick === "Stone Pickaxe"){
-                let copperrnd = Math.floor(Math.random()*3);
-                inv.copper.ore += copperrnd;
-                char.minexp += minexpgain;
-                mineGainEmbed.addField("Ores Mined", `Copper: +${copperrnd}`, true);
-                mineGainEmbed.addField("Mining EXP Gained", `+${minexpgain}`, true);
-                inv.save()
-                .then(result => console.log(result))
-                .catch(err => console.log(err));
-            }
-            message.channel.send(mineGainEmbed);
-            char.active = false;
-            char.save()
-            .then(result => console.log(result))
-            .catch(err => console.log(err));
-            setTimeout(() => {
-                if(char.minexp >= nextminelvl){
-                    char.minelvl = char.minelvl + 1;
-                    let lvlupEmbed = new Discord.RichEmbed()
-                    .setTitle("**Your mining skills leveled up!**")
-                    //.setThumbnail("../../images/greenarrow.png")
-                    .setColor("#32CD32")
-                    .setDescription(`${message.author.username}, you now have a mining skill level of ${char.minelvl-1}!`);
-                    message.channel.send(lvlupEmbed);
-                    char.save()
+            if (char.active === false) {
+                char.active = true;
+                char.save()
                     .then(result => console.log(result))
                     .catch(err => console.log(err));
+                if (inv.pick.toLowerCase() === "stone pickaxe") {
+                    inv.pickspeed = 250;
+                    inv.save()
+                        .then(result => console.log(result))
+                        .catch(err => console.log(err));
+                } else if (inv.pick.toLowerCase() === "copper pickaxe") {
+                    inv.pickspeed = 10000;
+                    inv.save()
+                        .then(result => console.log(result))
+                        .catch(err => console.log(err));
+                } else if (inv.pick.toLowerCase() === "iron pickaxe") {
+                    inv.pickspeed = 20000;
+                    inv.save()
+                        .then(result => console.log(result))
+                        .catch(err => console.log(err));
                 }
-            }, 1000);
-        }, 5000);
+                let minexprnd = Math.floor(Math.random() * 4);
+                let minexpgain = minexprnd + char.minelvl;
+                let nextminelvl = Math.floor(Math.pow(char.minelvl, 2.6));
+                let minespeed = 600250 - inv.pickspeed;
 
-    
-        ratelimitMap.set(message.author.id, Date.now() + 5000);
-    }else{
-        return message.reply("You are already doing something.");
-    }
+                let copperrnd = Math.floor(Math.random() * 1) + 3;
+                let ironrnd = Math.floor(Math.random() * 1) + 3;
+                let silverrnd = Math.floor(Math.random() * 1) + 2;
+                let nickelrnd = Math.floor(Math.random() * 1) + 3;
 
+                let silverchance = Math.floor(Math.random() * 4);
+                let nickelchance = Math.floor(Math.random() * 3);
+
+                let mineEmbed = new Discord.RichEmbed()
+                    .setTitle("Mining")
+                    .setColor("#855e42")
+                    .setDescription("You are off to the mines for a while!")
+                    .addField("Pickaxe", `${inv.pick.toUpperCase()}`);
+                message.channel.send(mineEmbed);
+
+
+                let mineGainEmbed = new Discord.RichEmbed()
+                    .setAuthor("Mining")
+                    .setTitle("**You are back from mining!**")
+                    .setColor("#855e42");
+
+                let oremined = [];
+
+
+                setTimeout(() => {
+                    if (inv.pick.toLowerCase() === "stone pickaxe") {
+                        inv.copper.ore += copperrnd;
+                        oremined.push(`Copper Ore +${copperrnd}`);
+                        char.minexp += minexpgain;
+                        mineGainEmbed.addField("Ores Mined", `${oremined.join("\n")}`, true);
+                        mineGainEmbed.addField("Mining EXP Gained", `+${minexpgain}`, true);
+                        inv.save()
+                            .then(result => console.log(result))
+                            .catch(err => console.log(err));
+                    } else if (inv.pick.toLowerCase() === "copper pickaxe") {
+                        inv.copper.ore += copperrnd;
+                        oremined.push(`Copper Ore +${copperrnd}`);
+                        inv.iron.ore += ironrnd;
+                        oremined.push(`Iron Ore +${ironrnd}`);
+                        char.minexp += minexpgain;
+                        mineGainEmbed.addField("Ores Mined", `${oremined.join("\n")}`, true);
+                        mineGainEmbed.addField("Mining EXP Gained", `+${minexpgain}`, true);
+                        inv.save()
+                            .then(result => console.log(result))
+                            .catch(err => console.log(err));
+                    } else if (inv.pick.toLowerCase() === "iron pickaxe") {
+                        inv.copper.ore += copperrnd;
+                        oremined.push(`Copper Ore +${copperrnd}`);
+                        inv.iron.ore += ironrnd;
+                        oremined.push(`Iron Ore +${ironrnd}`);
+                        if(silverchance === 0){
+                            inv.silver.ore += silverrnd;
+                            oremined.push(`Silver Ore: +${silverrnd}`);
+                        } else if (nickelchance === 1){
+                            inv.nickel.ore += nickelrnd;
+                            oremined.push(`Nickel Ore: +${nickelrnd}`);
+                        }
+                        char.minexp += minexpgain;
+                        mineGainEmbed.addField("Ores Mined", `${oremined.join("\n")}`, true);
+                        mineGainEmbed.addField("Mining EXP Gained", `+${minexpgain}`, true);
+                        inv.save()
+                            .then(result => console.log(result))
+                            .catch(err => console.log(err));
+                    }
+                    message.channel.send(mineGainEmbed);
+                    char.active = false;
+                    char.save()
+                        .then(result => console.log(result))
+                        .catch(err => console.log(err));
+                    setTimeout(() => {
+                        if (char.minexp >= nextminelvl) {
+                            char.minelvl = char.minelvl + 1;
+                            let lvlupEmbed = new Discord.RichEmbed()
+                                .setTitle("**Your mining skills leveled up!**")
+                                //.setThumbnail("../../images/greenarrow.png")
+                                .setColor("#32CD32")
+                                .setDescription(`${message.author.username}, you now have a mining skill level of ${char.minelvl-1}!`);
+                            message.channel.send(lvlupEmbed);
+                            char.save()
+                                .then(result => console.log(result))
+                                .catch(err => console.log(err));
+                        }
+                    }, 1000);
+                }, minespeed);
+
+            
+                ratelimitMap.set(message.author.id, Date.now() + minespeed);
+            } else {
+                return message.reply("You are already doing something.");
+            }
+
+        });
     });
-});
-}
+    }
 
 module.exports.help = {
     name: "mine"
