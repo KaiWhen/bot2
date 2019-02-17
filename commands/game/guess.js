0b32ace56f56960892ee2aa32665c83353ca8fb0
KaiWhen <wangkw523@gmail.com>
2019-02-17 20:58:07 +0000

    Update guess.js

--- commands/game/guess.js
+++ commands/game/guess.js
@@ -4,20 +4,20 @@ const mongoose = require("mongoose");
 
 module.exports.run = async(bot, message, args) => {
 
-    let words = [{
-        word: "Hello",
-        defn: "used as a greeting or to begin a telephone conversation."
-    }, {
-        word: "Goodbye",
-        defn: "used to express good wishes when parting or at the end of a conversation."
-    }];
+    let guessgame = {words: [
+        {word: "Hello", defn: "used as a greeting or to begin a telephone conversation."},
+        {word: "Goodbye", defn: "used to express good wishes when parting or at the end of a conversation."}
+    ]
+    };
+
+    let wrd = guessgame.words;
 
     let guessEmbed = new Discord.RichEmbed()
     .setTitle("Guess the word!");
 
-    let wordrnd = Math.random(Math.floor()*words.length);
+    let wordrnd = Math.random(Math.floor()*wrd.length);
     
-    guessEmbed.setDescription(`Definition: ${words[wordrnd].defn}`);
+    guessEmbed.setDescription(`Definition: ${wrd[wordrnd].defn}`);
 
     message.channel.send(guessEmbed);
 
@@ -28,7 +28,7 @@ module.exports.run = async(bot, message, args) => {
 
         let guess = collected.first().content;
 
-        if(guess.toUpperCase() === words[wordrnd].name){
+        if(guess.toUpperCase() === wrd[wordrnd].name){
 
             message.channel.send(`${message.author.username} guessed the word right!`);
 
